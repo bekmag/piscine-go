51c3557e7b8c844226495ce8229473f8735cda86
bekmag <bekmag@github.com>
2019-10-16 23:33:35 +0600

    My seventeenth

--- printcomb2.go
+++ printcomb2.go
@@ -2,7 +2,7 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintComb() {
+func PrintComb2() {
 	for a := '0'; a <= '9'; a++ {
 		for b := '0'; b <= '9'; b++ {
 			for c := '0'; c <= '9'; c++ {
@@ -30,4 +30,5 @@ func PrintComb() {
 			}
 		}
 	}
+	z01.PrintRune(10)
 }
