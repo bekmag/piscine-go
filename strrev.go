63cf9c68e16733eb8e2d990a87d7607c1cafd199
bekmag <bekmag@github.com>
2019-10-18 00:19:42 +0600

    My ninth 17.10

---
+++ strrev.go
@@ -0,0 +1,14 @@
+package piscine
+
+func StrRev(s string) string {
+	runes := []rune(s)
+	var a rune
+	for j := range runes {
+		}
+	for i := 0; i < (j+1)/2; i++ {
+		a = runes[i]
+		runes[i] = runes [j-1]
+		runes[j-1] = a
+	}
+	return string(runes)
+}
