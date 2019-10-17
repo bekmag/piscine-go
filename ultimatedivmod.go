05bc65c7cf012802f4dbe58d25b2b14e6ffbc7a0
bekmag <bekmag@github.com>
2019-10-17 21:36:17 +0600

    My fourth 17.10

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -1,9 +1,10 @@
 package piscine
 
 func UltimateDivMod(a *int, b *int) {
-	var x, y *int
-	*a = *x / *y
-	*b = *x % *y
-	*x = *a
-	*y = *b
+	var div int
+	var mod int
+	div := *a / *b
+	mod := a % b
+	*a = div
+	*b = mod
 }
