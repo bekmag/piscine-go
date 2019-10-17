0b986f2776a6a8249de909558deea357334ac962
bekmag <bekmag@github.com>
2019-10-17 23:25:52 +0600

    My sixth 17.10

--- printstr.go
+++ printstr.go
@@ -3,8 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
-	slice := []rune(str)
-	for index, word := range slice {
-		z01.PrintRune(runes(index))
+	slice := 0
+	for i := range slice {
 	}
 }
