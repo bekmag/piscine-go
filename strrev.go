1c0ac8a7ea313d0b93dfe20e1e642af11ef4d5c3
bekmag <bekmag@github.com>
2019-10-18 00:20:44 +0600

    My ninth 17.10

--- strrev.go
+++ strrev.go
@@ -4,10 +4,10 @@ func StrRev(s string) string {
 	runes := []rune(s)
 	var a rune
 	for j := range runes {
-		}
+	}
 	for i := 0; i < (j+1)/2; i++ {
 		a = runes[i]
-		runes[i] = runes [j-1]
+		runes[i] = runes[j-1]
 		runes[j-1] = a
 	}
 	return string(runes)
