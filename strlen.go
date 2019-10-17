206c2b91ad7d3a4c3c493b1e2c5ac11bf6797446
bekmag <bekmag@github.com>
2019-10-17 23:55:27 +0600

    My seventh 17.10

--- strlen.go
+++ strlen.go
@@ -5,8 +5,7 @@ import "github.com/01-edu/z01"
 func StrLen(str string) int {
 	n := 0
 	runes := []rune(str)
-	for i := range runes int {
-	n++
+	for n := range runes int {
 	}
-	return n
+	return n + 1
 }
