ad0702e97ce7d7a666b51731d500aa18644c9c68
bekmag <bekmag@github.com>
2019-10-19 16:16:52 +0600

    My printnbr

--- printnbr.go
+++ printnbr.go
@@ -3,21 +3,13 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-	a := '-'
-	b := '1'
-	c := '2'
-	d := '3'
-	e := '0'
+	a := []rune(n)
+	b := range(a)
 	if n < 0 {
-		z01.PrintRune(a)
 		z01.PrintRune(b)
-		z01.PrintRune(c)
-		z01.PrintRune(d)
 	} else if n == 0 {
-		z01.PrintRune(e)
+		z01.PrintRune(b)
 	} else {
 		z01.PrintRune(b)
-		z01.PrintRune(c)
-		z01.PrintRune(d)
 	}
 }
