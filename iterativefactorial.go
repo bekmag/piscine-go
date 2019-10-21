c4890ff0285e7f0c6480abd316f8eaa81daf0682
bekmag <bekmag@github.com>
2019-10-21 15:24:31 +0600

    My 4.1

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,7 +3,7 @@ package piscine
 func IterativeFactorial(nb int) int {
 	result := 1
 	resultb := 0
-	if nb > 0 && nb < 2147483647 {
+	if nb >= 0 && nb < 2147483647 {
 		for i := 1; i <= nb; i++ {
 			result = result * i
 		}
