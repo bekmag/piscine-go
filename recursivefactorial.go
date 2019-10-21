2ca2a95cd3688d7f0193ebb4dc2142af951c9c96
bekmag <bekmag@github.com>
2019-10-21 15:50:41 +0600

    My 4.2

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,10 +1,10 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb == 1 {
+	if nb == 1 && nb < 2147483647 {
 		return 1
 	}
-	if nb > 1 {
+	if nb > 1 && nb < 2147483647 {
 		return nb * RecursiveFactorial(nb-1)
 	}
 	return 0
