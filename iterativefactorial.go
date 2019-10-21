448d13e3ffdad1a5ff4aa60759f49213a067dc64
bekmag <bekmag@github.com>
2019-10-21 20:33:52 +0600

    My 4.5

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,14 +2,11 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	resultb := 0
 	if nb >= 0 && nb < 2147483647 {
 		for i := 1; i <= nb; i++ {
 			result = result * i
 		}
 		return result
 	}
-	if nb > 0 && nb < 2147483647 {
-	}
-	return resultb
+	return 0
 }
--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,11 +3,9 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var aRune rune = 'z'
-	for aRune >= 'a' {
-
-		z01.PrintRune(aRune)
-		aRune--
+	for a := 122; a >= 97; {
+		z01.PrintRune(rune(a))
+		a--
 	}
 	z01.PrintRune(10)
 }
