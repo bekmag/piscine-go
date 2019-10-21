764222e7ea95521aec41771cf7a17adf6f3ebb13
bekmag <bekmag@github.com>
2019-10-21 17:57:34 +0600

    My 4.3

---
+++ iterativepower.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	result := 1
+	if nb >= 0 && nb < 2147483647 {
+		for i := 1; i <= power; i++ {
+			result = result * nb
+		}
+		return result
+	}
+	return 0
+}
