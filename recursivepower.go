a09d29b689f6cdfe84d7c303d239e01cf09c105f
bekmag <bekmag@github.com>
2019-10-21 20:18:18 +0600

    My 4.4

--- recursivepower.go
+++ recursivepower.go
@@ -1,17 +1,11 @@
 package piscine
 
 func RecursivePower(nb int, power int) int {
-	if nb < 0 && power < 0 {
-		return 0
-	}
-	if nb == 1 && power == 1 {
-		return 1
-	}
-	if nb == 0 && power == 0 {
+	if power == 0 {
 		return 1
 	}
 	if nb > 0 && power > 0 {
 		return nb * RecursivePower(nb, power-1)
 	}
-	return power + 1
+	return 0
 }
