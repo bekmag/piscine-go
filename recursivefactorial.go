55c4701dc2d3283a5bdab69742dac850e0ad4695
bekmag <bekmag@github.com>
2019-10-21 15:52:05 +0600

    My 4.2

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -4,7 +4,7 @@ func RecursiveFactorial(nb int) int {
 	if nb == 1 && nb < 2147483647 {
 		return 1
 	}
-	if nb > 1 && nb < 2147483647 {
+	if nb > 0 && nb < 2147483647 {
 		return nb * RecursiveFactorial(nb-1)
 	}
 	return 0
