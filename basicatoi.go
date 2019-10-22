9aeac1580ae2319fd2894953768bb4f5d48d900d
bekmag <bekmag@github.com>
2019-10-22 19:39:39 +0600

    My basicatoi

---
+++ basicatoi.go
@@ -0,0 +1,31 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	var result int = 0
+	runes := []rune(s)
+	for _, j := range runes {
+		result *= 10
+		if j == '0' {
+			result += 0
+		} else if j == '1' {
+			result += 1
+		} else if j == '2' {
+			result += 2
+		} else if j == '3' {
+			result += 3
+		} else if j == '4' {
+			result += 4
+		} else if j == '5' {
+			result += 5
+		} else if j == '6' {
+			result += 6
+		} else if j == '7' {
+			result += 7
+		} else if j == '8' {
+			result += 8
+		} else if j == '9' {
+			result += 9
+		}
+	}
+	return result
+}
