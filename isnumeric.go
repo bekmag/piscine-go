1c82da0e520197ca41f73daebea82e060de8298a
bekmag <bekmag@github.com>
2019-10-23 21:57:33 +0600

    My 5.12

---
+++ isnumeric.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsNumeric(str string) bool {
+	runes := []rune(str)
+	for a := range runes {
+		if (runes[a] >= 33 && runes[a] <= 47) || (runes[a] >= 58 && runes[a] <= 64) || (runes[a] >= 91 && runes[a] <= 96) || (runes[a] >= 123 && runes[a] <= 126) || runes[a] == ' ' {
+			return false
+		}
+	}
+	return true
+}
