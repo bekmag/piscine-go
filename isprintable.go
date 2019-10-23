2697108647d53143f8d4e210e8a024df7aa81858
bekmag <bekmag@github.com>
2019-10-23 22:09:10 +0600

    My 5.15

--- isprintable.go
+++ isprintable.go
@@ -3,9 +3,9 @@ package piscine
 func IsPrintable(str string) bool {
 	runes := []rune(str)
 	for a := range runes {
-		if (runes[a] >= 65 && runes[a] <= 90) || (runes[a] >= 48 && runes[a] <= 57) || (runes[a] >= 97 && runes[a] <= 122) || (runes[a] >= 33 && runes[a] <= 47) || (runes[a] >= 58 && runes[a] <= 64) || (runes[a] >= 91 && runes[a] <= 96) || (runes[a] >= 123 && runes[a] <= 126) || runes[a] == ' ' {
-			return true
+		if runes[a] >= 0 && runes[a] <= 32 {
+			return false
 		}
 	}
-	return false
+	return true
 }
