fe2124da19c1ab9d7228acef3d678a32660de6b2
bekmag <bekmag@github.com>
2019-10-24 21:09:37 +0600

    My 7.4

---
+++ splitwhitespaces.go
@@ -0,0 +1,32 @@
+package piscine
+
+func SplitWhiteSpaces(str string) []string {
+	count := 0
+	strFound := false
+
+	for char := range str {
+		if char == ' ' || char == '\n' || char == '\t' {
+			strFound = false
+		} else if !strFound {
+			strFound = true
+			count++
+		}
+	}
+
+	result := make([]string, count)
+
+	fillIndex := -1
+	strFound = false
+	for char := range str {
+		if char == ' ' || char == '\n' || char == '\t' {
+			strFound = false
+		} else {
+			if !strFound {
+				fillIndex++
+			}
+			strFound = true
+			result[fillIndex] += string(char)
+		}
+	}
+	return result
+}
